bc8a2a4f1377cd5a58f7eac1ae619f5c0a5f2c5d
Szepesi Tibor <sztibi023@gmail.com>
2022-01-04 23:36:45 +0100

    feat: handle SIGTERM gracefully

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod utils;
 
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use hyper::{body::Buf, Body, Method, Request, Response, Server, StatusCode};
-use std::{convert::Infallible, net::SocketAddr, time::Duration};
+use std::{convert::Infallible, future::Future, io, net::SocketAddr, time::Duration};
+use tokio::signal::unix::{signal, SignalKind};
 use tower::make::Shared;
 use tower_http::add_extension::AddExtensionLayer;
 
@@ -50,8 +51,17 @@ async fn handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(res)
 }
 
+fn sigterm_signal() -> io::Result<impl Future<Output = ()>> {
+    let mut stream = signal(SignalKind::terminate())?;
+
+    Ok(async move {
+        stream.recv().await;
+        tracing::info!("Got SIGTERM, shutting down");
+    })
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> io::Result<()> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .with_test_writer()
@@ -70,6 +80,9 @@ async fn main() {
     tracing::info!("Listening on port {}", addr.port());
     Server::bind(&addr)
         .serve(Shared::new(service))
+        .with_graceful_shutdown(sigterm_signal()?)
         .await
         .expect("server error");
+
+    Ok(())
 }
