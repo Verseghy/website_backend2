b32b2c25f1344090d33bf6a87912d9417957f6a1
Szepesi Tibor <sztibi023@gmail.com>
2022-01-06 02:08:30 +0100

    refactor: switch to actix_web

--- src/main.rs
+++ src/main.rs
@@ -3,73 +3,50 @@ mod entity;
 mod graphql;
 mod utils;
 
+use actix_web::{
+    web::{self, Data},
+    App, HttpResponse, HttpServer, Responder,
+};
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
-use hyper::{body::Buf, Body, Method, Request, Response, Server, StatusCode};
-use std::{convert::Infallible, future::Future, io, net::SocketAddr, time::Duration};
-use tokio::signal::unix::{signal, SignalKind};
-use tower::make::Shared;
-use tower_http::add_extension::AddExtensionLayer;
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+use std::io;
 
 use crate::graphql::create_schema;
 use crate::graphql::Schema;
 use crate::utils::num_threads;
 
 const GRAPHQL_PATH: &str = "/graphql";
+const PORT: u16 = 3000;
 
-async fn handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let schema = req.extensions().get::<Schema>().unwrap().clone();
-    let mut res = Response::new(Body::empty());
+async fn graphql(schema: Data<Schema>, request: GraphQLRequest) -> GraphQLResponse {
+    schema.execute(request.into_inner()).await.into()
+}
 
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, GRAPHQL_PATH) => {
-            let source = playground_source(GraphQLPlaygroundConfig::new(GRAPHQL_PATH));
-            *res.body_mut() = Body::from(source);
-        }
-        (&Method::POST, GRAPHQL_PATH) => {
-            *res.status_mut() = StatusCode::BAD_REQUEST;
+async fn graphql_playground() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(playground_source(GraphQLPlaygroundConfig::new(
+            GRAPHQL_PATH,
+        )))
+}
 
-            let body = hyper::body::to_bytes(req.into_body()).await;
-            if let Ok(body) = body {
-                let reader = body.reader();
-                if let Ok(request) = serde_json::from_reader::<_, async_graphql::Request>(reader) {
-                    let response = schema.execute(request).await;
-                    let response_json = serde_json::to_string(&response).unwrap();
+async fn readiness() -> HttpResponse {
+    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+    HttpResponse::Ok().body("")
+}
 
-                    *res.body_mut() = Body::from(response_json);
-                    *res.status_mut() = StatusCode::OK;
-                }
-            }
+async fn liveness() -> HttpResponse {
+    if let Ok(threads) = num_threads().await {
+        if threads < 10000 {
+            tracing::debug!("Liveness thread count: {}", threads);
+            return HttpResponse::Ok().body("");
         }
-        (&Method::GET, "/readiness") => {
-            *res.status_mut() = StatusCode::OK;
-        }
-        (&Method::GET, "/liveness") => {
-            if let Ok(threads) = num_threads().await {
-                if threads < 10000 {
-                    *res.status_mut() = StatusCode::OK;
-                } else {
-                    *res.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
-                }
-            } else {
-                *res.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
-            }
-        }
-        _ => *res.status_mut() = StatusCode::NOT_FOUND,
     }
 
-    Ok(res)
+    HttpResponse::InternalServerError().body("")
 }
 
-fn sigterm_signal() -> io::Result<impl Future<Output = ()>> {
-    let mut stream = signal(SignalKind::terminate())?;
-
-    Ok(async move {
-        stream.recv().await;
-        tracing::info!("Got SIGTERM, shutting down");
-    })
-}
-
-#[tokio::main]
+#[actix_web::main]
 async fn main() -> io::Result<()> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
@@ -78,20 +55,18 @@ async fn main() -> io::Result<()> {
         .compact()
         .init();
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     let schema = create_schema().await;
 
-    let service = tower::ServiceBuilder::new()
-        .timeout(Duration::from_secs(30))
-        .layer(AddExtensionLayer::new(schema))
-        .service_fn(handler);
-
-    tracing::info!("Listening on port {}", addr.port());
-    Server::bind(&addr)
-        .serve(Shared::new(service))
-        .with_graceful_shutdown(sigterm_signal()?)
-        .await
-        .expect("server error");
-
-    Ok(())
+    tracing::info!("Listening on port {}", PORT);
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(schema.clone()))
+            .route(GRAPHQL_PATH, web::post().to(graphql))
+            .route(GRAPHQL_PATH, web::get().to(graphql_playground))
+            .route("/readiness", web::get().to(readiness))
+            .route("/liveness", web::get().to(liveness))
+    })
+    .bind(("0.0.0.0", PORT))?
+    .run()
+    .await
 }
