2709766869e828c5e06a3198a9fc035487c3c372
Szepesi Tibor <sztibi023@gmail.com>
2022-01-09 14:37:43 +0100

    migrations: fix canteen_data date

--- migrations/03-canteen_data_add_time.sql
+++ migrations/03-canteen_data_add_time.sql
@@ -1,4 +1,11 @@
 UPDATE `canteen_data` SET
-	`date`=ADDTIME(`date`, "01:00:00"),
-	`created_at`=ADDTIME(`created_at`, "01:00:00"),
-	`updated_at`=ADDTIME(`updated_at`, "01:00:00");
+	`date`       = ADDTIME(`date`, "02:00:00"),
+	`created_at` = ADDTIME(`created_at`, "02:00:00"),
+	`updated_at` = ADDTIME(`updated_at`, "02:00:00")
+	WHERE DATE_FORMAT(`date`, '%H') = 22;
+
+UPDATE `canteen_data` SET
+	`date`       = ADDTIME(`date`, "01:00:00"),
+	`created_at` = ADDTIME(`created_at`, "01:00:00"),
+	`updated_at` = ADDTIME(`updated_at`, "01:00:00")
+	WHERE DATE_FORMAT(`date`, '%H') = 23;
