24e91ae6fb7ff4117aa8085d3326eac0cf2f5ae8
Szepesi Tibor <sztibi023@gmail.com>
2022-07-07 21:27:21 +0200

    refactor: only use DATABASE_URL

--- src/database.rs
+++ src/database.rs
@@ -1,21 +1,10 @@
 use sea_orm::{ConnectOptions, Database, DatabaseConnection};
-use std::env::{var, VarError};
-
-fn get_db_uri() -> Result<String, VarError> {
-    let username = var("DB_USERNAME")?;
-    let password = var("DB_PASSWORD")?;
-    let host = var("DB_HOST")?;
-    let db = var("DB_DATABASE")?;
-
-    Ok(format!("mysql://{}:{}@{}/{}", username, password, host, db))
-}
 
 pub async fn connect() -> DatabaseConnection {
-    let uri = get_db_uri().expect("Could not create database URI");
-
     tracing::info!("Trying to connect to database");
 
-    let opts = ConnectOptions::new(uri);
+    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL not set");
+    let opts = ConnectOptions::new(url);
     let db = Database::connect(opts).await.unwrap();
 
     tracing::info!("Connected to database");
