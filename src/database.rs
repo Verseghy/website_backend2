645d806edbc8f099567d5165b10c795563b9c8b0
Szepesi Tibor <smrtrfszm@proton.me>
2026-01-17 14:06:43 +0100

    Use envconfig

--- src/database.rs
+++ src/database.rs
@@ -1,10 +1,9 @@
 use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 use tracing::log::LevelFilter;
 
-pub async fn connect() -> DatabaseConnection {
+pub async fn connect(url: &str) -> DatabaseConnection {
     tracing::info!("Trying to connect to database");
 
-    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL not set");
     let mut opts = ConnectOptions::new(url);
     opts.sqlx_logging_level(LevelFilter::Debug);
     let db = Database::connect(opts).await.unwrap();
